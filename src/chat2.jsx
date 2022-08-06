014ae505da632782c4103ed4676fbaf451b17ef0
Sonu Saini <sonu2311saini@gmail.com>
2022-08-06 14:21:19 +0530

    create new chat page

--- src/chat2.jsx
+++ src/chat2.jsx
@@ -4,87 +4,80 @@ import React from 'react';
 import {api} from './library';
 import {SessionContext} from './library';
 
-export function ChatSection({focusedPersonId}){
 
-  const [chatList , setChatList] =React.useState([])
+export function ChatSection2({focusedPersonId2}){
   const [session, setSession] = React.useContext(SessionContext)
+  const [chatList , setChatList] =React.useState([])
   const [content, setContent] = React.useState("")
   const [oldestId, setOldestId] = React.useState(0)
   const newestId = React.useRef(0)
   const stoper = React.useRef(false)
 
-  // "user_id": 3, "older_than": None, "limit": 20
   const keepLoadingNewmessages = function(){
-    setTimeout(()=> {
+    setTimeout(()=>{
       if (stoper.current){
-        return 
+        return
       }
-      api("/get_new_messages", {"user_id":focusedPersonId,"newer_than": newestId.current }, function(backend_output) {
+      api("/get_new_messages", {"user_id":focusedPersonId2,"newer_than": newestId.current }, function(backend_output) {
+        console.log(backend_output)
+        setChatList(chatList.concat(backend_output.messages))
+        // setChatList([...chatList, ...backend_output.messages])
         newestId.current = (backend_output.newest_id)
-        setChatList(x => x.concat(backend_output.messages))
-        console.log("backend_output==", backend_output)
         keepLoadingNewmessages()
       })
-    },2000 ) 
-  }
+    }, 3000)
 
+  }
 
   React.useEffect(()=> {
 
-    api("/get_old_messages", {"user_id":focusedPersonId,"older_than": null, "limit": 3  }, function(backend_output) {
+    api("/get_old_messages", {"user_id":focusedPersonId2,"older_than": null, "limit": 3  }, function(backend_output) {
       console.log(backend_output)
       setChatList(backend_output.messages)
       setOldestId(backend_output.oldest_id)
       newestId.current = (backend_output.newest_id)
       keepLoadingNewmessages()
-
     })
-    return () => {
+    return () =>{
       stoper.current = true
-      console.log("khatam ho gya====")
     }
+     
+    
   }, []);
 
   const send_message= function(){
-    api("/send_message", {"receiver_id":focusedPersonId,"content":content }, function(backend_output) {
+    api("/send_message", {"receiver_id":focusedPersonId2,"content":content }, function(backend_output) {
       console.log(backend_output)
       setContent("")
       
     })
   }
   const loadOldMessages =function(){
-    api("/get_old_messages", {"user_id":focusedPersonId,"older_than": oldestId, "limit": 3  }, function(backend_output) {
+    api("/get_old_messages", {"user_id":focusedPersonId2,"older_than": oldestId, "limit": 3  }, function(backend_output) {
       console.log(backend_output)
-      setChatList(backend_output.messages.concat(chatList))
+      // setChatList(backend_output.messages.concat(chatList))
+      setChatList([...backend_output.messages, ...chatList])
       setOldestId(backend_output.oldest_id)
     })
   }
 
-  const loadNewMessages =function(){
-    api("/get_new_messages", {"user_id":focusedPersonId,"newer_than": newestId.current }, function(backend_output) {
-      console.log(backend_output)
-      setChatList(chatList.concat(backend_output.messages))
-      newestId.current = (backend_output.newest_id)
-    })
-  }
+  // const loadNewMessages =function(){
+  //   api("/get_new_messages", {"user_id":focusedPersonId2,"newer_than": newestId.current }, function(backend_output) {
+  //     console.log(backend_output)
+  //     setChatList(chatList.concat(backend_output.messages))
+  //     // setChatList([...chatList, ...backend_output.messages])
+  //     newestId.current = (backend_output.newest_id)
+  //   })
+  // }
 
-//  $timeout(function() {
-//     document.getElementById("chat_scroll_container").scrollTo(0, 10000);
-//   }, 0);
-  // "id": 5,
-  //     "sender_id": 3,
-  //     "receiver_id": 1,
-  //     "content": "M5",
-  //     "chat_time": 1658923614,
-  //     "name": "Monu",
-  //     "chat_time_str": "2022-07-27 05:36:54 PM"
 
   return (
     <>
       <div>
+       
         <div style={{backgroundColor: '#128C7E', color: '#fff', padding: '15px', border: 'solid #ccc 1px', fontSize: '18px', fontWeight: 500}}>
           <div style={{width: '50%', whiteSpace:'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'}}>
-            {"----"}</div>
+            {"----99999"}</div>
         </div>
         <div style={{backgroundImage: 'url("https://user-images.githubusercontent.com/15075759/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.png")', height: 'calc( 100vh - 195px)', overflowY: 'scroll'}} id="chat_scroll_container">
           <div style={{padding: '10px 25px'}}>
@@ -93,23 +86,25 @@ export function ChatSection({focusedPersonId}){
               <button className='oldMessagesbutton'  onClick={loadOldMessages}>load old messages</button>
             </div>
             
-            {chatList.map((x)=>(
+            {chatList.map((chat)=>(
               <div className="hsplit" style={{padding: '3px 0px'}}>
                 
-                <div className="chat_div" style={{float: (x.sender_id==session.login_key.id) ? "right":"left",backgroundColor: (x.sender_id==session.login_key.id) ? "rgb(220, 248, 198)":"#fff"}}>
+                <div className="chat_div" style={{float: (chat.sender_id==session.login_key.id) ? "right":"left",backgroundColor: (chat.sender_id==session.login_key.id) ? "rgb(220, 248, 198)":"#fff"}}>
                   <div style={{fontSize: '12px', color: 'blue', paddingBottom: '5px'}}>
-                    {x.name}
+                    {chat.name}
                   </div>
-                  <div>[{x.id}] {x.content}</div>
+                  <div>[{chat.id}] {chat.content}</div>
                   <div style={{float: 'right', fontSize: '12px', color: '#3a7f48', paddingTop: '5px'}}>
-                    {x.chat_time_str}
+                    {chat.chat_time_str}
                   </div>
                 </div>
 
               </div>
             ))}
           </div>
+          {/* <button onClick={loadNewMessages}>new messages</button> */}
         </div>
+       
         <div style={{height: '50px', verticalAlign: 'middle', padding: '1px 15px'}}>
 
           <textarea style={{borderRadius: '35px', padding: '10px 20px', float: 'left', width: 'calc(100% - 80px)', height: '20px', outline: 'none', resize: 'none', border: '1px solid grey', marginBottom: '5px', marginTop: '10px'}} placeholder="Comment" value={content} onChange={(e)=>setContent(e.target.value)} />
--- src/home.jsx
+++ src/home.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 import { ChatSection } from './chat';
 import { LeftPanel } from './left_panel';
 import { useParams } from 'react-router-dom';
+import { ChatSection2 } from './chat2';
 
 function Home(){
   const {id}= useParams()
@@ -15,9 +16,14 @@ function Home(){
             <div className="left_panel">
               <LeftPanel id={id} />
             </div>
-            <div className='mid_panel'>
+            {/* <div className='mid_panel'>
               {id>0 &&(
                 <ChatSection focusedPersonId={id}  key={id}/>
+              )}
+            </div> */}
+            <div className='mid_panel'>
+              {id>0 &&(
+                <ChatSection2 focusedPersonId2={id}  key={id}/>
               )}
             </div>
           </div>
